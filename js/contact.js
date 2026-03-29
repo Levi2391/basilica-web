7e43b75ffd0684cf898255855076f289a3363300
Irving CG <deock1@gmail.com>
2026-03-29 19:53:45 +0200

    feature: contact js logic file added

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+function loadContactHTML() {
+    removeHistoriaButtons();
+
+    document.getElementById("panel").style.display = "block";
+    document.getElementById("mainVideo").pause();
+
+    canvas.style.display = "none";
+
+    const container = document.getElementById("contact-container");
+    console.log(container);
+    container.style.display = "grid";
+    container.innerHTML = "Cargando...";
+    console.log(container);
+    container.innerHTML = `
+        <img class="img-contained" src="./imagenes/contacto.jpg"></img>
+      `;
+
+    currentView = "horarios";
+
+    container.classList.remove("hidden");
+}
