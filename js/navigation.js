66e07343c1e88bf5204b3144c4c737bac6c24bea
Irving CG <deock1@gmail.com>
2026-04-18 19:58:03 +0200

    refactor: main menu structure and logic

--- js/navigation.js
+++ js/navigation.js
@@ -19,3 +19,21 @@ function goBack() {
         goHome();
     }
 }
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+
+    if (menu.classList.contains("menu-open")) {
+        menu.classList.remove("menu-open");
+        menu.classList.add("menu-collapsed");
+    } else {
+        menu.classList.remove("menu-collapsed");
+        menu.classList.add("menu-open");
+    }
+}
+
+function closeMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.classList.remove("menu-open");
+    menu.classList.add("menu-collapsed");
+}
