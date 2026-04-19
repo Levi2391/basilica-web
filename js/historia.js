afa04fafc57a62010ac16fc11295e4584b076c2c
Irving CG <deock1@gmail.com>
2026-04-19 22:44:55 +0200

    fix: minor styles layout

--- js/historia.js
+++ js/historia.js
@@ -41,7 +41,7 @@ function showHistoria(sectionId) {
                 </div>`;
 
         if (block.type === "image-text")
-            return `<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-8 mb-12 items-center">
+            return `<div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-12 items-center">
                     <img src="${block.img}" class="w-full">
                     <p>${block.p}</p>
                 </div>`;
